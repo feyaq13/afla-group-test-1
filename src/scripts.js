db8cb8305deef8ba5878f9cf3328547a891f83f1
feyaq13 <feyaq13@gmail.com>
2020-04-07 21:11:41 +0300

    дорабатывает модель

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,44 @@
-const fields = document.querySelectorAll('.form-choose-licence-plan__field');
-fields.forEach(field => field.addEventListener('click', selectCurrent));
+const planFields = document.querySelectorAll('.form-choose-licence-plan__field');
+planFields.forEach(field => field.addEventListener('click', selectCurrent));
 
-var current = Array.from(fields).find(function (field) {
-  if (field.classList.contains('focus')) {
-    return field
-  }
+var currentPlanField = Array.from(planFields).find(function (field) {
+  return field.classList.contains('focus');
 });
 
+
 function selectCurrent(e) {
-  if (current) {
-    current.classList.remove('focus');
+  if (currentPlanField) {
+    currentPlanField.classList.remove('focus');
   }
 
   e.currentTarget.querySelector('input').checked = true;
   e.currentTarget.classList.add('focus');
-  current = e.currentTarget;
+  currentPlanField = e.currentTarget;
+  document.querySelector('.choose-plan > span').innerText = currentPlanField.dataset.index;
 }
+
+const totalPriceElement = document.querySelector('.total-price');
+const numberOfLicenses = document.querySelector('select');
+let numberOfLicensesValue = numberOfLicenses.selectedIndex + 1;
+
+let licensePrice = Array.from(planFields);
+console.log(Array.from(planFields));
+
+// .find(childNode => childNode.dataset?.value).dataset.value;
+
+numberOfLicenses.addEventListener('change', function() {
+  numberOfLicensesValue = numberOfLicenses.selectedIndex + 1;
+  calculate()
+});
+
+planFields.forEach((field, i ) => field.addEventListener('click', function (e) {
+  licensePrice = e.currentTarget.querySelector('span').dataset.value;
+  calculate()
+}));
+
+function calculate() {
+  totalPriceElement.value = numberOfLicensesValue * licensePrice;
+}
+
+calculate();
 
