17c9a6e67c6957fec655bd0dc3be21049ae64dbf
feyaq13 <feyaq13@gmail.com>
2020-04-07 21:10:01 +0300

    начинает преобразовывать radio fields в модель

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,57 @@
-const fields = document.querySelectorAll('.form-choose-licence-plan__field');
-fields.forEach(field => field.addEventListener('click', selectCurrent));
+var planFieldsObject = function () {
+  const fields = document.querySelectorAll('.form-choose-licence-plan__field');
+  var currentPlanField = function findElementInFocus() {
+    Array.from(fields).find(function (field) {
+      return field.classList.contains('focus');
+    });
+  };
 
-var current = Array.from(fields).find(function (field) {
-  if (field.classList.contains('focus')) {
-    return field
+  return {
+    fields: fields,
+    currentPlanField: currentPlanField
   }
-});
+}();
+
+// const planFields = document.querySelectorAll('.form-choose-licence-plan__field');
+planFieldsObject.fields.forEach(field => field.addEventListener('click', selectCurrent));
+
+var currentPlanField = planFieldsObject.currentPlanField();
+
+// console.log(planFieldsObject.fields);
 
 function selectCurrent(e) {
-  if (current) {
-    current.classList.remove('focus');
+  if (planFieldsObject.currentPlanField()) {
+    currentPlanField.classList.remove('focus');
   }
 
   e.currentTarget.querySelector('input').checked = true;
   e.currentTarget.classList.add('focus');
-  current = e.currentTarget;
+  currentPlanField = e.currentTarget;
+  document.querySelector('.choose-plan > span').innerText = currentPlanField.dataset.index;
 }
+
+const totalPriceElement = document.querySelector('.total-price');
+const numberOfLicenses = document.querySelector('select');
+let numberOfLicensesValue = numberOfLicenses.selectedIndex + 1;
+
+let licensePrice = 34;
+// console.log(Array.from(planFields));
+
+// .find(childNode => childNode.dataset?.value).dataset.value;
+
+numberOfLicenses.addEventListener('change', function() {
+  numberOfLicensesValue = numberOfLicenses.selectedIndex + 1;
+  calculate()
+});
+
+planFieldsObject.fields.forEach((field, i ) => field.addEventListener('click', function (e) {
+  licensePrice = e.currentTarget.querySelector('span').dataset.value;
+  calculate()
+}));
+
+function calculate() {
+  totalPriceElement.value = numberOfLicensesValue * licensePrice;
+}
+
+calculate();
 
